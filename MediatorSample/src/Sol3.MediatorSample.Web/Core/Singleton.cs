f988165fbc22a6d467b20d6d13483cac8625725d
Keith Barrows <kbarrows@weissinc.com>
2019-04-09 16:55:11 -0400

    Stepping towards a logger

---
+++ MediatorSample/src/Sol3.MediatorSample.Web/Core/Singleton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sol3.MediatorSample.Web.Core
+{
+    /// <summary>
+    /// Inherit from this class to create a singleton instance of your class.  
+    /// <para><seealso cref="http://www.dofactory.com/net/singleton-design-pattern"/></para>
+    /// <para>NOTE:  &lt;T&gt; is the same class that inherits this object!</para>
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// public MyClass : Singleton{MyClass}
+    /// </code>
+    /// </example>
+    /// <typeparam name="T">The class that is implementing this base class</typeparam>
+    public class Singleton<T> where T : new()
+    {
+        protected static readonly Lazy<T> Lazy = new Lazy<T>(() => new T());
+
+        public static T Instance => Lazy.Value;
+
+        protected Singleton() { }
+    }
+}
