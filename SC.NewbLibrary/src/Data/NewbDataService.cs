5e69eabd1abf39b6a2c916e21fbefbd7deb6bc45
Keith Barrows <keith.barrows@sol3.net>
2020-03-03 19:26:57 -0500

    Add project files via upload

---
+++ SC.NewbLibrary/src/Data/NewbDataService.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SC.NewbLibrary.Data
+{
+    public class NewbDataService
+    {
+        public Task<NewbData[]> GetNewbData()
+        {
+            // https://stackoverflow.com/questions/52279980/blazor-read-directory
+
+            var rootPath = Directory.GetCurrentDirectory();
+            var filePath = Path.Combine(rootPath, "Data\\NEWB.json");
+            var file = File.ReadAllText(filePath);
+            var obj = JsonConvert.DeserializeObject<NewbData[]>(file);
+            return Task.FromResult(obj);
+        }
+    }
+}
