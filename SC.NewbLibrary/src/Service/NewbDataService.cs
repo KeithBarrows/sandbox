f67a6e284c5aa262a7b1f11f1b33f7687f882add
KeithBarrows <Keith.Barrows@geogroupconsultants.com>
2020-03-04 16:26:13 -0500

    Event source updates

--- SC.NewbLibrary/src/Service/NewbDataService.cs
+++ SC.NewbLibrary/src/Service/NewbDataService.cs
@@ -1,21 +1,35 @@
 ﻿using Newtonsoft.Json;
 using SC.NewbLibrary.Model.Data;
+using SC.NewbLibrary.Model.View;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace SC.NewbLibrary.Service
 {
     public class NewbDataService
     {
-        public Task<NewbData[]> GetNewbData()
+        public Task<NewbViewData[]> GetNewbData()
         {
             // https://stackoverflow.com/questions/52279980/blazor-read-directory
 
             var rootPath = Directory.GetCurrentDirectory();
             var filePath = Path.Combine(rootPath, "wwwroot/NEWB.json");
             var file = File.ReadAllText(filePath);
-            var obj = JsonConvert.DeserializeObject<NewbData[]>(file);
-            return Task.FromResult(obj);
+            var newbData = JsonConvert.DeserializeObject<NewbData[]>(file);
+
+            var result = new List<NewbViewData>();
+            newbData.ToList().ForEach(a => result.Add(new NewbViewData
+            {
+                Definition = a.Definition,
+                Link = a.Link,
+                Term = a.Term,
+                Terse = a.Terse,
+                EventHistory = a.EventHistory
+            }));
+
+            return Task.FromResult(result.ToArray());
         }
     }
 }
