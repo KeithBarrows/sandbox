dc30c6635eb5344ca7c3e3cba04cbbcff80bed90
KeithBarrows <Keith.Barrows@geogroupconsultants.com>
2020-03-04 12:06:34 -0500

    Reorganize code

---
+++ SC.NewbLibrary/src/Service/NewbDataService.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using SC.NewbLibrary.Model.Data;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace SC.NewbLibrary.Service
+{
+    public class NewbDataService
+    {
+        public Task<NewbData[]> GetNewbData()
+        {
+            // https://stackoverflow.com/questions/52279980/blazor-read-directory
+
+            var rootPath = Directory.GetCurrentDirectory();
+            var filePath = Path.Combine(rootPath, "wwwroot/NEWB.json");
+            var file = File.ReadAllText(filePath);
+            var obj = JsonConvert.DeserializeObject<NewbData[]>(file);
+            return Task.FromResult(obj);
+        }
+    }
+}
