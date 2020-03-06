2b244b32632c8d285197b515e9acc8693343eb53
KeithBarrows <Keith.Barrows@geogroupconsultants.com>
2020-03-06 15:55:13 -0500

    Start of state engine

--- SC.NewbLibrary/src/Model/Data/NewbData.cs
+++ SC.NewbLibrary/src/Model/Data/NewbData.cs
@@ -9,9 +9,9 @@ namespace SC.NewbLibrary.Model.Data
         [StringLength(50)]
         [Key]
         public string Term { get; set; }
-        public string Definition { get; set; }
-        public string Terse { get; set; }
-        public string Link { get; set; }
+        //public string Definition { get; set; }
+        //public string Terse { get; set; }
+        //public string Link { get; set; }
 
         public List<NewbDataEvent> EventHistory { get; set; } = new List<NewbDataEvent>();
     }
