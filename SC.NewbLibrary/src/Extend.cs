538745de9703237924a0dae99087f97ec78cee09
Keith Barrows <keith.barrows@sol3.net>
2020-03-06 00:47:55 -0500

    Read done for now.  Todo: Filters, Add, Update

---
+++ SC.NewbLibrary/src/Extend.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SC.NewbLibrary
+{
+    public static class Extend
+    {
+        public static bool IsEmpty(this string src) => string.IsNullOrWhiteSpace(src);
+        public static bool IsEqualTo(this string src, string tgt)
+        {
+            src = src ?? "";
+            tgt = tgt ?? "";
+            return src.Equals(tgt, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
