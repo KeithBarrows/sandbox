a53a5674e870b2601ae11c28aad17e240772fe47
Keith Barrows <keith.barrows@sol3.net>
2021-01-13 19:57:20 -0500

    More updates.  Playing with layout

--- sol3-web/src/app/app.module.ts
+++ sol3-web/src/app/app.module.ts
@@ -8,6 +8,13 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+//import {MatNativeDateModule} from '@angular/material/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {Sol3MaterialModule} from './material-model'
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -18,9 +25,19 @@ import { MenuComponent } from './menu/menu.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    Sol3MaterialModule,
+    //MatNativeDateModule,
+    //ReactiveFormsModule,
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
