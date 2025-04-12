936a42c63e1c2340072453ba7765018099bb055b
Keith Barrows <keith.barrows@sol3.net>
2025-04-12 00:34:00 -0400

    chore: upgrade Angular dependencies to version 19.x and update related packages
    feat: refactor AboutComponent to use CommonModule and MatExpansionModule
    
    fix: update AppComponent to include RouterModule in imports
    
    refactor: move MenuComponent to AppModule imports and clean up declarations
    
    feat: update ContactComponent to use CommonModule and MenuComponent
    
    feat: update HomeComponent to use CommonModule and MenuComponent
    
    fix: update polyfills to import zone.js correctly

--- Angular/sol3-web/src/app/home/home.component.ts
+++ Angular/sol3-web/src/app/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  imports: [MenuComponent, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeComponent implements OnInit {
 
